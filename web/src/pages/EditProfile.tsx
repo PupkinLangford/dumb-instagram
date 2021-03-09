da30c1dd98e11125725b0765c7d080b339f0dd8d
PupkinLangford <tahellman@gmail.com>
2021-03-09 11:44:25 -0500

    edit profile form layout

--- web/src/pages/EditProfile.tsx
+++ web/src/pages/EditProfile.tsx
@@ -1,20 +1,22 @@
 import React, {useState, useEffect, useRef, MouseEvent} from 'react';
 import {Link, useHistory} from 'react-router-dom';
-import {Formik} from 'formik';
+import {ErrorMessage, Field, Form, Formik} from 'formik';
 import config from '../config';
 import {useAuth} from '../hooks/use_auth';
 import styles from './EditProfile.module.css';
-import {useMutation} from '@apollo/client';
+import {useMutation, useQuery} from '@apollo/client';
 import {
   mutation_changeProfilePic,
   mutation_deleteProfilePic,
 } from '../graphql/mutations/user';
+import {query_current_user} from '../graphql/queries/user';
 
 const EditProfile = () => {
   const [auth, loadingAuth] = useAuth();
   const [showModal, setShowModal] = useState(false);
   const [changeProfilePic] = useMutation(mutation_changeProfilePic);
   const [deleteProfilePic] = useMutation(mutation_deleteProfilePic);
+  const {loading: queryLoading, data: queryData} = useQuery(query_current_user);
   const fileOnChange = async (files: FileList) => {
     try {
       const {data} = await changeProfilePic({variables: {picture: files[0]}});
@@ -36,9 +38,12 @@ const EditProfile = () => {
   };
 
   useEffect(() => {
+    console.log(queryData);
     if (showModal) document.body.style.overflow = 'hidden';
     else document.body.style.overflow = '';
   }, [showModal]);
+
+  if (queryLoading) return <div></div>;
 
   const modal = (
     <div className={styles.cover} onClick={() => setShowModal(false)}>
@@ -85,6 +90,42 @@ const EditProfile = () => {
               </button>
             </div>
           </div>
+          <Formik
+            initialValues={{
+              firstName: queryData.current_user.first_name,
+              lastName: queryData.current_user.last_name,
+              bio: queryData.current_user.bio,
+              email: queryData.current_user.email,
+              emailConfirm: queryData.current_user.email,
+            }}
+            onSubmit={values => console.table(values)}
+          >
+            <Form>
+              <label htmlFor="firstName">First Name</label>
+              <Field name="firstName" type="text"></Field>
+              <ErrorMessage name="firstName"></ErrorMessage>
+
+              <label htmlFor="lastName">Last Name</label>
+              <Field name="lastName" type="text"></Field>
+              <ErrorMessage name="lastName"></ErrorMessage>
+
+              <label htmlFor="bio">Bio</label>
+              <Field name="bio" component="textarea"></Field>
+              <ErrorMessage name="bio"></ErrorMessage>
+
+              <label htmlFor="email">Email</label>
+              <Field name="email" type="email"></Field>
+              <ErrorMessage name="email"></ErrorMessage>
+
+              <label htmlFor="emailConfirm">Confirm Email</label>
+              <Field name="emailConfirm" type="text"></Field>
+              <ErrorMessage name="emailConfirm"></ErrorMessage>
+
+              <button type="submit" id={styles.submitButton}>
+                Submit
+              </button>
+            </Form>
+          </Formik>
         </article>
       </main>
       {showModal ? modal : null}
