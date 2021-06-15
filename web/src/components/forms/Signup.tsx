a9ce6b0aba3c87e2bf171e1330e066a7a5c53be2
PupkinLangford <tahellman@gmail.com>
2021-06-15 11:11:37 -0500

    factor Signup into separate form component

--- web/src/components/forms/Signup.tsx
+++ web/src/components/forms/Signup.tsx
@@ -2,50 +2,106 @@ import React from 'react';
 import {ErrorMessage, Field, Form, Formik} from 'formik';
 import styles from './Forms.module.css';
 import {useMutation} from '@apollo/client';
-import {mutation_changePassword} from '../../graphql/mutations/user';
-import {changePasswordRules} from '../../rules/rules';
+import {mutation_signup, mutation_login} from '../../graphql/mutations/user';
+import {signUpRules} from '../../rules/rules';
+import {GraphQLError} from 'graphql';
+import {setLogin} from '../../utils';
+import {useHistory} from 'react-router-dom';
 
 const Signup = () => {
-  const [changePassword] = useMutation(mutation_changePassword);
+  const [signUp] = useMutation(mutation_signup, {errorPolicy: 'all'});
+  const [userLogin] = useMutation(mutation_login, {errorPolicy: 'all'});
+  const history = useHistory();
   return (
     <Formik
       initialValues={{
+        username: '',
         password: '',
         passwordConfirm: '',
+        email: '',
       }}
-      validationSchema={changePasswordRules}
-      onSubmit={async (values, {resetForm}) => {
-        changePassword({
+      validationSchema={signUpRules}
+      onSubmit={async (values, {setErrors}) => {
+        const {data, errors} = await signUp({
           variables: {
+            username: values.username,
             password: values.password,
+            email: values.email,
             passwordConfirm: values.passwordConfirm,
           },
         });
-        alert('Password changed');
-        resetForm();
+        if (errors) {
+          errors.forEach(error => {
+            setErrors({
+              username: ((error.message as unknown) as GraphQLError).message,
+            });
+          });
+          return;
+        } else {
+          const {data: loginData} = await userLogin({
+            variables: {
+              username: data.signup.username,
+              password: values.password,
+            },
+          });
+          setLogin(loginData.login.token, loginData.login.user);
+          history.push('/');
+        }
       }}
-      key="changePassword"
+      key="signUp"
     >
-      <Form className={styles.settingsForm}>
-        <label htmlFor="password">New Password</label>
-        <Field name="password" type="password"></Field>
+      <Form className={styles.loginForm}>
+        <Field id="email" name="email" type="email" placeholder="Email" />
+        <ErrorMessage name="email" component="div" className={styles.errors} />
+        <Field
+          id="username"
+          name="username"
+          type="text"
+          placeholder="Username"
+        />
+        <ErrorMessage
+          name="username"
+          component="div"
+          className={styles.errors}
+        />
+        <Field
+          id="password"
+          name="password"
+          type="password"
+          placeholder="Password"
+        />
         <ErrorMessage
           name="password"
           component="div"
           className={styles.errors}
         />
-
-        <label htmlFor="passwordConfirm">Confirm New Password</label>
-        <Field name="passwordConfirm" type="password"></Field>
+        <Field
+          id="passwordConfirm"
+          name="passwordConfirm"
+          type="password"
+          placeholder="Confirm Password"
+        />
         <ErrorMessage
           name="passwordConfirm"
           component="div"
           className={styles.errors}
         />
-
-        <button type="submit" id={styles.submitButton}>
-          Submit
-        </button>
+        <button type="submit">Sign Up</button>
+        <p className={styles.disclaimer}>
+          By signing up, you agree to our{' '}
+          <abbr title="A series of probably unenforceable rules backed by a binding arbitration clause">
+            Terms
+          </abbr>
+          ,{' '}
+          <abbr title="Any public or private data on any of your devices belongs to us">
+            Data Policy
+          </abbr>{' '}
+          and{' '}
+          <abbr title="User shall not bake any raisin-based cookies">
+            Cookies Policy
+          </abbr>{' '}
+          .
+        </p>
       </Form>
     </Formik>
   );
--- web/src/pages/Login.tsx
+++ web/src/pages/Login.tsx
@@ -1,122 +1,21 @@
 import React, {useState} from 'react';
 import {useHistory} from 'react-router-dom';
-import {ErrorMessage, Field, Form, Formik} from 'formik';
 import {useAuth} from '../hooks/use_auth';
 import logo from '../images/logo.png';
 import icon from '../images/di_icon.png';
-import {signUpRules} from '../rules/rules';
 import './Login.module.css';
 import styles from './Login.module.css';
-import {useMutation} from '@apollo/client';
-import {mutation_login, mutation_signup} from '../graphql/mutations/user';
-import {GraphQLError} from 'graphql';
-import {setLogin} from '../utils';
 import LoginForm from '../components/forms/LoginForm';
+import Signup from '../components/forms/Signup';
 
 const Login = () => {
   const [showLogin, setShowLogin] = useState(true);
   const [auth, loadingAuth] = useAuth();
-  const [signUp] = useMutation(mutation_signup, {errorPolicy: 'all'});
-  const [userLogin] = useMutation(mutation_login, {errorPolicy: 'all'});
   const history = useHistory();
 
   if (!loadingAuth && auth) {
     history.push('/');
   }
-
-  const signupForm = (
-    <Formik
-      initialValues={{
-        username: '',
-        password: '',
-        passwordConfirm: '',
-        email: '',
-      }}
-      validationSchema={signUpRules}
-      onSubmit={async (values, {setErrors}) => {
-        const {data, errors} = await signUp({
-          variables: {
-            username: values.username,
-            password: values.password,
-            email: values.email,
-            passwordConfirm: values.passwordConfirm,
-          },
-        });
-        if (errors) {
-          errors.forEach(error => {
-            setErrors({
-              username: ((error.message as unknown) as GraphQLError).message,
-            });
-          });
-          return;
-        } else {
-          const {data: loginData} = await userLogin({
-            variables: {
-              username: data.signup.username,
-              password: values.password,
-            },
-          });
-          setLogin(loginData.login.token, loginData.login.user);
-          history.push('/');
-        }
-      }}
-      key="signUp"
-    >
-      <Form className={styles.loginForm}>
-        <Field id="email" name="email" type="email" placeholder="Email" />
-        <ErrorMessage name="email" component="div" className={styles.errors} />
-        <Field
-          id="username"
-          name="username"
-          type="text"
-          placeholder="Username"
-        />
-        <ErrorMessage
-          name="username"
-          component="div"
-          className={styles.errors}
-        />
-        <Field
-          id="password"
-          name="password"
-          type="password"
-          placeholder="Password"
-        />
-        <ErrorMessage
-          name="password"
-          component="div"
-          className={styles.errors}
-        />
-        <Field
-          id="passwordConfirm"
-          name="passwordConfirm"
-          type="password"
-          placeholder="Confirm Password"
-        />
-        <ErrorMessage
-          name="passwordConfirm"
-          component="div"
-          className={styles.errors}
-        />
-        <button type="submit">Sign Up</button>
-        <p className={styles.disclaimer}>
-          By signing up, you agree to our{' '}
-          <abbr title="A series of probably unenforceable rules backed by a binding arbitration clause">
-            Terms
-          </abbr>
-          ,{' '}
-          <abbr title="Any public or private data on any of your devices belongs to us">
-            Data Policy
-          </abbr>{' '}
-          and{' '}
-          <abbr title="User shall not bake any raisin-based cookies">
-            Cookies Policy
-          </abbr>{' '}
-          .
-        </p>
-      </Form>
-    </Formik>
-  );
 
   return (
     <div className={styles.login}>
@@ -129,7 +28,7 @@ const Login = () => {
                 id={styles.logo}
                 style={{backgroundImage: `url(${logo})`}}
               ></div>
-              {showLogin ? <LoginForm /> : signupForm}
+              {showLogin ? <LoginForm /> : <Signup />}
             </div>
             <div className={styles.switch}>
               {showLogin ? (
